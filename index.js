ddf967b2ef7332362558c55fd94cb04ef5429a70
arbic <teo.maybebaby@gmail.com>
2025-03-08 14:53:21 +0500

    eskiz refreshToken added

--- index.js
+++ index.js
@@ -49,8 +49,10 @@ app.post('/', (req, res) => {
 })
 
 const httpsOptions = {
-	key: fs.readFileSync('./etc/letsencrypt/live/mses-chat.uz/privkey.pem'),
-	cert: fs.readFileSync('./etc/letsencrypt/live/mses-chat.uz/fullchain.pem'),
+	key: fs.readFileSync('./etc/letsencrypt/live/mses-chat.uz-0001/privkey.pem'),
+	cert: fs.readFileSync(
+		'./etc/letsencrypt/live/mses-chat.uz-0001/fullchain.pem'
+	),
 }
 
 const server = https.createServer(httpsOptions, app)
